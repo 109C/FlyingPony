912f9bf0ffd71335e7b2af21fb900111240c6e3e
C109 <yemerrynonsense@gmail.com>
2016-01-03 14:50:33 -0500

    Improved inheritance

--- util/Inheritance.js
+++ util/Inheritance.js
@@ -1,9 +1,24 @@
-module.exports = function(Parent, Child){
+module.exports = Inheritance
+
+function Inheritance(Parent, Child){
     for(key in Parent){
         Child[key] = Parent[key]
     }
     
-    var InheritanceChain = Parent.__inheritance__ || "[Root]"
-    
-    Child.__inheritance__ = InheritanceChain + " -> " + Child.constructor.name
+    // Init child inheritance if it doesn't already have a parent.
+    if(Child.__inheritance == undefined){
+        Child.__inheritance__ = {parents:[], name: Child.constructor.name}
+    }
+    // Add parent to the child's parents.
+    if(Parent.__inheritance__ == undefined){
+        Child.__inheritance__.parents.push({parents:[], name: "[Root]"})
+    }else{
+        Child.__inheritance__.parents.push(Parent.__inheritance__)
+    }
+}
+
+Inheritance.pprint = pprint
+
+function pprint(Child){
+    return JSON.stringify(Child.__inheritance__, null, 2)
 }
